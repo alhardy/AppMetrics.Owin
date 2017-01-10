bc37ae572b2d41e99d32ac40dc60c06e6b572c4b
al_hardy@live.com.au <al_hardy@live.com.au>
2017-01-10 20:56:41 +1100

    dont attempt to run tests now, none exist

--- scripts/default.ps1
+++ scripts/default.ps1
@@ -22,7 +22,7 @@ $appProjects = Get-ChildItem "$srcRoot\**\$projectFileName" | foreach { $_.FullN
 $testProjects = Get-ChildItem "$testsRoot\**\$projectFileName" | foreach { $_.FullName }
 $packableProjectDirectories = @("$srcRoot\App.Metrics.Extensions.Owin")
 
-task default -depends PatchProject, TestParams, Setup, Build, RunTests, Pack
+task default -depends PatchProject, TestParams, Setup, Build, Pack
 
 task TestParams { 
 	Assert ($configuration -ne $null) '$configuration should not be null'
